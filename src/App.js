7d6a8ada4e5a18fc5497a2616002d98f098c583e
Hazem1234321 <hazemmohsen188@gmail.com>
2024-05-20 15:56:12 +0300

    adding pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Shop from './pages/Shop';
 function App() {
   return (
     <div >
+      <BrowserRouter>
       <Navbar/>
+      
+      <Routes>
+        <Route path='/' element = {<Shop/>}/>
+      
+
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
